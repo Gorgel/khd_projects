9870ab613b2c425aa3dca20c0488901f48562e10
Simon Karlsson <skrotupplag@hotmail.com>
2015-02-05 12:26:19 +0100

    removed likes, added math suppport, removed content

--- khd_projects/projects/templatetags/markdown_filter.py
+++ khd_projects/projects/templatetags/markdown_filter.py
@@ -2,6 +2,8 @@ __author__ = 'gorgel'
 
 import markdown2
 
+from markdown2Mathjax import sanitizeInput, reconstructMath
+from markdown2 import markdown
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.encoding import force_unicode
@@ -12,7 +14,19 @@ register = template.Library()
 @register.filter(is_safe=True)
 @stringfilter
 def custom_markdown(value):
-    extras = ["fenced-code-blocks"]
 
-    return mark_safe(markdown2.markdown(force_unicode(value),
-                                       extras = extras))
+    # thanks to https://github.com/constantAmateur/markdown2Mathjax
+    extras = ["fenced-code-blocks"]
+    tmp = sanitizeInput(value)
+    markedDownText = markdown2.markdown(force_unicode(tmp[0]), extras = extras)
+    finalOutput = reconstructMath(markedDownText,tmp[1])
+    return mark_safe(finalOutput)
+
+#old working code, save for now.
+#@register.filter(is_safe=True)
+#@stringfilter
+#def custom_markdown(value):
+#    extras = ["fenced-code-blocks"]
+#
+#    return mark_safe(markdown2.markdown(force_unicode(value),
+#                                      extras = extras))
